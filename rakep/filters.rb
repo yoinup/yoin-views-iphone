4d0897577de0b195461f87836072b901d7154102
Pepe Cano <ppcanodehuelva@gmail.com>
2013-03-09 20:40:06 +0100

    Using Borders for iOS and Android

--- rakep/filters.rb
+++ rakep/filters.rb
@@ -7,6 +7,7 @@ module Yoin
 
       super(options, context, &block)
       @options = { :paths => ["#{Yoin.submodule_path}yoin-views-iphone/less/import", 
+                              "#{Yoin.submodule_path}yoin-views/less/import-ios", 
                               "#{Yoin.submodule_path}yoin-views/less/import", 
                               "#{Yoin.submodule_path}yoin-views/less/import2"] }
     end
